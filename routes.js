14d1339c79a99c8ce9a3f9cdcbfa2336049873fb
harrison-symes <zanelockett@gmail.com>
2018-08-17 10:16:00 +1200

    routes and styling

--- routes.js
+++ routes.js
@@ -16,4 +16,13 @@ router.get('/restaurants/submit', function(req,res){
     res.render('restaurants/submit', data)
 })
 
+router.get('/restaurants/:id', (req, res) => {
+    const restId = req.params.id
+    const found = data.restaurants.find(function(rests) {
+      return rests.id == restId;
+    });
+
+    res.render('restaurants/view', found)
+  })
+
 module.exports = router

cb5ed47edf322777382dc5a80eb7816177e422bb
kyokopl <kmpohe@gmail.com>
2018-08-16 18:12:03 +1200

    CSS is working

--- routes.js
+++ routes.js
@@ -4,11 +4,14 @@ const data = require('./data.json')
 const fs = require('fs')
 
 router.get('/', function(req,res){
-    res.send("Hi FEDA")
+    res.redirect('restaurants')
 })
 
 router.get('/restaurants', function(req,res){
     res.render('restaurants/index', data)
 })
+// router.get('/restaurants', function(req,res){
+//     res.render('restaurants/index', data)
+// })
 
 module.exports = router
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.engine('hbs', hbs({
 server.set('view engine', 'hbs')
 server.use(express.static('public'))
 server.use(express.urlencoded({extended: false}))
-server.use('/', routes)
 
+server.use('/', routes)
 
 module.exports = server

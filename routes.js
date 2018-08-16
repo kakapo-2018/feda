12b8590ff8acfd65573ff92bafdeab2394315e57
student <yuzuki.sadoh@gmail.com>
2018-08-17 11:36:30 +1200

    post route

--- routes.js
+++ routes.js
@@ -16,6 +16,22 @@ router.get('/submit', function(req,res){
     res.render('restaurants/submit', data)
 })
 
+router.post('/restaurants', function(req, res)  {
+    let restaurantSubmit = req.body
+
+    let restaurantNextId = data.restaurants.length+1
+    restaurantid = restaurantNextId
+    data.restaurants.push(restaurantSubmit)
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+        console.log('err', err)
+        if (err) res.send('Not OK')
+        if (!err) res.redirect('/')   
+
+    })    
+})
+
+
 router.get('/restaurants/:id', (req, res) => {
     const restId = req.params.id
     const found = data.restaurants.find(function(rests) {
@@ -24,6 +40,8 @@ router.get('/restaurants/:id', (req, res) => {
 
     res.render('restaurants/view', found)
   })
+
+
   
 
 module.exports = router

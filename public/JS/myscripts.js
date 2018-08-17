5e096ac9c7d4ba3ba319c795108a1275a2d01b80
harrison-symes <zanelockett@gmail.com>
2018-08-17 12:10:38 +1200

    home page is working

--- public/JS/myscripts.js
+++ public/JS/myscripts.js
@@ -1,17 +1,56 @@
-
 document.addEventListener('DOMContentLoaded', createEventListeners)
+let data = require('../../data.json')
+const fs = require('fs')
 
+// announcement section
 function createEventListeners (){
     images = document.getElementById("note");
     images.addEventListener("mousedown", pushedImage);
     images.addEventListener("mouseup", backImage);
 } 
 
- function pushedImage () {
+function pushedImage () {
 this.className = "pushed"
 console.log("hello")
 }
 
 function backImage () {
 this.className = "";
+}
+
+// function ranking() {
+//     let arr = data.restaurants.map(function(r) {
+//         let ratingStars = r.rating
+//         console.log(ratingStars)
+//         return ratingStars
+//     }).sort((a,b) => b.length-a.length);
+//     console.log(arr)   
+// }
+
+// ranking ()
+
+// funciton ratingRestaurants() {
+//     return data.json sorted by the number of stars
+// }
+
+
+function ranking() {
+    let arr = data.restaurants.map(function(r) {
+        let ratingStars = r.rating
+
+        console.log(ratingStars)
+        return ratingStars 
+    }).sort((a,b) => b.length-a.length);
+    console.log(arr)   
+}
+
+// rankRestaurants()
+// rating system
+
+
+// numArray.sort((a, b) => b - a); // For descending sort
+
+
+module.exports = {
+   ranking
 }
